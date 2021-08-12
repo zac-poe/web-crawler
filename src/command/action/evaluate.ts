b6c9c807860b537b37a760c5afa4f9bcc2e51191
zac-poe <zacharypoe@gmail.com>
2021-08-12 00:07:23 -0400

    implementing evaluate command

--- src/command/action/evaluate.ts
+++ src/command/action/evaluate.ts
@@ -1,5 +1,9 @@
 import { Command } from "../command";
 import { Action, ActionContext } from "./action";
+import { parse as parseHtml } from 'parse5';
+import { select } from 'xpath';
+import { serializeToString as toXml } from 'xmlserializer';
+import { DOMParser } from 'xmldom';
 
 export class EvaluateAction extends Action {
     getCommand(): string {
@@ -7,6 +11,13 @@ export class EvaluateAction extends Action {
     }
 
     run(context: ActionContext): Promise<any> {
-        return super.run(context);
+        if(typeof context.value !== 'object') {
+            return Promise.reject(`${this.getCommand()} requires key/values`);
+        }
+        const xmlDocument = new DOMParser().parseFromString(toXml(parseHtml(context.state[Command[Command.Get]] ?? '')));
+        for(const variable of Object.keys(context.value)) {
+            context.state[variable] = select(context.value[variable], xmlDocument);
+        }
+        return Promise.resolve(context.state);
     }
 }
--- test/command/action/evaluate.test.ts
+++ test/command/action/evaluate.test.ts
@@ -1,8 +1,21 @@
+import { ActionContext } from "../../../src/command/action/action";
 import { EvaluateAction } from "../../../src/command/action/evaluate";
 import { Command } from "../../../src/command/command";
 
 describe('evaluate action', () => {
+    const context = (expression: string, state: any={}): ActionContext => ({
+        value: expression,
+        state: state,
+        block: undefined as any
+    });
+
     it('has evaluate command', () => {
         expect(new EvaluateAction().getCommand()).toEqual(Command[Command.Evaluate]);
+    });
+
+    it('evaluates xpath against document', () => {
+    });
+
+    it('evaluates multiple variables', () => {
     });
 });
