9389a819ee0487ca420e286c001786861d2c992d
zac-poe <zacharypoe@gmail.com>
2021-08-14 23:34:03 -0400

    completing testing of commands rewrite

--- test/command/action/commands.test.ts
+++ test/command/action/commands.test.ts
@@ -6,14 +6,23 @@ import { CommandBlock } from "../../../src/command/command-block";
 jest.mock("../../../src/command/command-block");
 
 describe('commands action', () => {
+    const mockBlock = jest.fn();
+
     const context = (items: any[], state: any={}): ActionContext => ({
         value: items,
         state: state,
-        block: undefined as any
+        previousCommands: []
+    });
+
+    beforeAll(() => {
+        (CommandBlock as any).mockImplementation(() => ({
+            resolve: mockBlock
+        }));
     });
 
     beforeEach(() => {
         (CommandBlock as any).mockClear();
+        mockBlock.mockClear();
     })
 
     it('has commands command', () => {
@@ -25,41 +34,41 @@ describe('commands action', () => {
     });
 
     it('executes commands', async () => {
-        const sequence1 = {
+        const commands = [{
             a: 'value1',
             b: 'value2'
-        }, sequence2 = {
+        }, {
             c: 'value3',
             d: 'value4'
-        };
+        }];
 
-        await new CommandsAction().run(context([sequence1, sequence2]));
+        mockBlock.mockReturnValue(Promise.resolve())
 
-        expect((CommandBlock as any).mock.instances.length).toEqual(2);
-        expect((CommandBlock as any).mock.calls[0][0]).toStrictEqual(sequence1);
-        expect((CommandBlock as any).mock.calls[1][0]).toStrictEqual(sequence2);
+        await new CommandsAction().run(context(commands));
+
+        expect((CommandBlock as any).mock.instances.length).toEqual(1);
+        expect((CommandBlock as any).mock.calls[0][0]).toStrictEqual(commands);
+        expect(mockBlock.mock.calls.length).toEqual(1);
     });
 
-    it('passes state between commands items', async () => {
+    it('passes state with reset repeat to commands', async () => {
         const expectedState = { variable: 'some value' };
 
-        (CommandBlock as any).mockReturnValueOnce({
-            resolve: () => Promise.resolve(expectedState)
+        mockBlock.mockReturnValue(Promise.resolve())
+
+        await new CommandsAction().run(context([{a:1}, {b:2}], expectedState));
+
+        expect((CommandBlock as any).mock.calls[0][1]).toStrictEqual({
+            ...expectedState,
+            Repeat: 1
         });
-
-        await new CommandsAction().run(context([{a:1}, {b:2}]));
-
-        expect((CommandBlock as any).mock.instances.length).toEqual(2);
-        expect((CommandBlock as any).mock.calls[1][1]).toStrictEqual(expectedState);
     });
 
     it('returns original state after commands', async () => {
         const initialState = { a: 'some value' },
             sequenceResultState = { b: 1, c: 2 };
 
-        (CommandBlock as any).mockReturnValueOnce({
-            resolve: () => Promise.resolve(sequenceResultState)
-        });
+        mockBlock.mockReturnValue(Promise.resolve(sequenceResultState))
 
         const result = await new CommandsAction().run(context([{command:'val'}],
             initialState));
